ecbe9642871ec87e787740419b4d5cb192ca3fae
cobbyjn <justicenrt@gmail.com>
2023-02-20 09:10:40 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ *
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0 ; a < 10 ; a++)
+	putchar(a);
+
+	putchar('\n');
+
+	return (0);
+}
