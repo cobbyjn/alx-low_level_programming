388be0ea584113db645a0eb81a631449e8dce549
cobbyjn <justicenrt@gmail.com>
2023-02-20 13:17:52 -0800

    Patience

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,11 +9,11 @@ int main(void)
 	int num;
 
 	for (num = 0 ; num < 10 ; num++)
-	putchar(num + '0');
+	putchar(num + ',' + ' ' + '0');
 
-	putchar(',');
+	/*putchar(',')*/
 
-	putchar(' ');
+	/*putchar(' ');*/
 
 	putchar('\n');
 
