91ee4c7d5d050c7ca660cba68d8dac30acf83281
cobbyjn <justicenrt@gmail.com>
2023-02-20 11:05:49 -0800

    Patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ *
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0 ; num < 10 ; num++)
+	putchar(num);
+
+	putchar(',');
+
+	putchar(' ');
+
+	putchar('\n');
+
+	return (0);
+}
