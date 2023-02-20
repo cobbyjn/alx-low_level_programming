af1dd1d31455e1dd623a2c0645a3f657faee6ea2
cobbyjn <justicenrt@gmail.com>
2023-02-19 17:27:37 -0800

    Last

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ *
+ * Return: 0 when successful
+ */
+int main(void)
+{
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
