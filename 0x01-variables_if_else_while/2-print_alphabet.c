54ca1100442d58ea8f06ea7b92191a03dfb147bf
cobbyjn <justicenrt@gmail.com>
2023-02-20 02:39:05 -0800

    I sometimes

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - the main function
+ *
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
