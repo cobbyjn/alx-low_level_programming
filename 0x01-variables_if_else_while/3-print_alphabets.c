604f737efc400c41df8b80b2b34419f389f11aec
cobbyjn <justicenrt@gmail.com>
2023-02-20 03:05:49 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ *
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a' ; a <= 'z' ; a++)
+
+		putchar(a);
+
+	for (a = 'A' ; a <= 'Z' ; b++)
+
+		putchar(b);
+
+	putchar('\n');
+
+	return (0);
+
+}
