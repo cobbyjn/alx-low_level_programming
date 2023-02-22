4842504818284443bb94a01194b56f858227ba51
cobbyjn <justicenrt@gmail.com>
2023-02-22 01:53:43 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - the main function
+ */
+void print_alphabet_x10(void)
+{
+	char j;
+	int c = 0;
+
+	while (c++ <= 9)
+	{
+		for (j = 'a' ; j <= 'z' ; j++)
+		_putchar(a + '10');
+
+		_putchar('\n');
+	}
+}
