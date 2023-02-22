a406918d450fb50589e760548c186e230ae4ee5f
cobbyjn <justicenrt@gmail.com>
2023-02-22 04:12:32 -0800

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_sign - main function
+ *
+ * @n: to be checked
+ *
+ * Return: 1 if greater than zero
+ *
+ * 0 and prints 0 if n is zero
+ *
+ * -1 and prints - if less than zero
+ *
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
