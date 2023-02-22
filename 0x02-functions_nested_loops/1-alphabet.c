d96a9b1f62832b48577937a83e34d2c38e4c8728
cobbyjn <justicenrt@gmail.com>
2023-02-22 00:53:25 -0800

    I sometimes

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - the main function
+ *
+ * Return: 0 when successful
+ */
+void print_alphabet(void)
+{
+	char j;
+
+	for (j = 'a' ; j <= 'z' ; j++)
+		_putchar(j);
+
+		_putchar('\n');
+}
