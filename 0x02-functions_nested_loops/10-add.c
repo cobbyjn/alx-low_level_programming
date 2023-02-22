5c70134dc549a582d695bfefbc4388b579e9f719
cobbyjn <justicenrt@gmail.com>
2023-02-22 15:54:06 -0800

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - main function
+ *
+ * @a: parameter
+ *
+ * @b: another parameter
+ *
+ * Return: when successful
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
