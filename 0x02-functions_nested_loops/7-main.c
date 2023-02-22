79ba4f205db313dfbe49891599009ae2d78d4d38
cobbyjn <justicenrt@gmail.com>
2023-02-22 04:58:39 -0800

    Only 3 colors

---
+++ 0x02-functions_nested_loops/7-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int print_last_digit();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit - the main function
+ *
+ * @j: checks the value
+ *
+ * Return: when successful
+ *
+ */
+int print_last_digit(int j)
+{
+	int jus;
+
+	jus = j % 10;
+
+	if (j < 0)
+		jus = -j;
+
+	return (jus);
+}
