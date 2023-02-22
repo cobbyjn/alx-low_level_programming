2dcb20afd23455b37b2d45e61961148c0362ae28
cobbyjn <justicenrt@gmail.com>
2023-02-22 04:35:19 -0800

    Such thing

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - the main function
+ *
+ * @n: checks the value
+ *
+ * Return: when successful
+ *
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
