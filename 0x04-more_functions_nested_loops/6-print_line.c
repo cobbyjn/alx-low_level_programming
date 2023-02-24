fd3a8f9bd6b96fda67294555ce664174c5f233a4
cobbyjn <justicenrt@gmail.com>
2023-02-24 10:27:53 -0800

    Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_line - main function
+ *
+ * @n: checks the integer
+ *
+ * Return: when successful
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	else
+	{
+		int a;
+
+		for (a = 0 ; a <= n ; a++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
