71d0172a107e9cc433e499bc003e68e206b67835
cobbyjn <justicenrt@gmail.com>
2023-02-24 06:08:09 -0800

    Multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - main function
+ *
+ * @a: checks the char
+ *
+ * @b: checks the value
+ *
+ * Return: always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+
+	_putchar('\n');
+}
